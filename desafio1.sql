b114c00acf4de18cdb1ab9be56b99b5ee7a83613
Aline <eualine2017@gmail.com>
2022-01-11 15:51:50 -0300

    REquisito 1 passando no teste

--- desafio1.sql
+++ desafio1.sql
@@ -4,209 +4,199 @@ CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
-CREATE TABLE users(
-    user_id INT AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    age INT NOT NULL,
-    id_plan INT NOT NULL,
-    signature_date DATE NOT NULL,
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (id_plan) REFERENCES plans(id_plan),
-) engine = InnoDB;
+CREATE TABLE plano(
+	plano_id INT AUTO_INCREMENT PRIMARY KEY,
+    plano VARCHAR(20),
+    valor_plano DEC(3, 2)
+);
 
-CREATE TABLE plans(
-    id_plan INT AUTO_INCREMENT,
-    plan VARCHAR(50) NOT NULL,
-    plan_price DECIMAL(3,2) NOT NULL,
-    PRIMARY KEY (id_plan),
-) engine = InnoDB;
+INSERT INTO plano(plano, valor_plano) VALUES
+('gratuito', 0.00),
+('universitário', 5.99),
+('pessoal', 6.99),
+('familiar', 7.99);
 
-CREATE TABLE artists(
-    artist_id INT AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    PRIMARY KEY (artist_id),
-) engine = InnoDB;
+CREATE TABLE usuario(
+	usuario_id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(20),
+    idade INT,
+    plano_id INT,
+    data_assinatura DATETIME,
+    FOREIGN KEY(plano_id) REFERENCES plano(plano_id)
+);
+
+INSERT INTO usuario(usuario, idade, plano_id, data_assinatura) VALUES
+('Thati', 23, 1, '2019-10-20'),
+('Cintia', 35, 4, '2017-12-30'),
+('Bill', 20, 2, '2019-06-05'),
+('Roger', 45, 3, '2020-05-13'),
+('Norman', 58, 3, '2017-02-17'),
+('Patrick', 33, 4, '2017-01-06'),
+('Vivian', 26, 2, '2018-01-05'),
+('Carol', 19, 2, '2018-02-14'),
+('Angelina', 42, 4, '2018-04-29'),
+('Paul', 46, 4, '2017-01-17');
+
+CREATE TABLE artistas(
+	artista_id INT AUTO_INCREMENT PRIMARY KEY,
+    artista VARCHAR(20)
+);
+
+INSERT INTO artistas(artista) VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon'),
+('Tyler Isle'),
+('Fog');
 
 CREATE TABLE albuns(
-    album_id INT AUTO_INCREMENT,
-    album VARCHAR(100) NOT NULL,
-    artist_id INT NOT NULL,
-    launch INT NOT NULL,
-    PRIMARY KEY (album_id),
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-) engine = InnoDB;
+	album_id INT AUTO_INCREMENT PRIMARY KEY,
+    album VARCHAR(20),
+    artista_id INT,
+    ano_lancamento INT,
+    FOREIGN KEY(artista_id) REFERENCES artistas(artista_id)
+);
 
-CREATE TABLE musics(
-    music_id INT AUTO_INCREMENT,
-    music VARCHAR(100) NOT NULL,
-    album_id INT NOT NULL,
-    time INT NOT NULL,
-    PRIMARY KEY (music_id),
-    FOREIGN KEY (album_id) REFERENCES albuns(album_id),
-) engine = InnoDB;
+INSERT INTO albuns(album, artista_id, ano_lancamento) VALUES
+('Envious', 1, 1990),
+('Exuberant', 1, 1993),
+('Hallowed Steam', 2, 1995),
+('Incandescent', 3, 1998),
+('Temporary Culture', 4, 2001),
+('Library of liberty', 4, 2003),
+('Chained Down', 5, 2007),
+('Cabinet of fools', 5, 2012),
+('No guarantees', 5, 2015),
+('Apparatus', 6, 2015);
 
-CREATE TABLE following(
-    artist_id INT,
-    user_id INT
-    PRIMARY KEY (artist_id, user_id),
-    FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-) engine = InnoDB;
+CREATE TABLE cancoes(
+	cancoes_id INT AUTO_INCREMENT PRIMARY KEY,
+    cancoes VARCHAR(50),
+    duracao_segundos INT,
+    album_id INT,
+    FOREIGN KEY(album_id) REFERENCES albuns(album_id)
+);
 
-CREATE TABLE history(
-    user_id INT NOT NULL,
-    music_id INT NOT NULL,
-    reprodution_date DATETIME NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (music_id) REFERENCES musics(music_id),
-) engine = InnoDB;
+INSERT INTO cancoes(cancoes, duracao_segundos, album_id) VALUES
+('Soul For Us', 200, 1),
+('Reflections Of Magic', 163, 1),
+('Dance With Her Own', 116, 1),
+('Troubles Of My Inner Fire', 203, 2),
+('Time Fireworks', 152, 2),
+('Magic Circus', 105, 3),
+('Honey, So Do I', 207, 3),
+('Sweetie, Lets Go Wild', 139, 3),
+('She Knows', 244, 3),
+('Fantasy For Me', 100, 4),
+('Celebration Of More', 146, 4),
+('Rock His Everything', 223, 4),
+('Home Forever', 231, 4),
+('Diamond Power', 241, 4),
+('Lets Be Silly', 132, 4),
+('Thang Of Thunder', 240, 5),
+('Words Of Her Life', 185, 5),
+('Without My Streets', 176, 5),
+('Need Of The Evening', 190, 6),
+('History Of My Roses', 222, 6),
+('Without My Love', 111, 6),
+('Walking And Game', 123, 6),
+('Young And Father', 197, 6),
+('Finding My Traditions', 179, 7),
+('Walking And Man', 229, 7),
+('Hard And Time', 135, 7),
+('Honey, Im A Lone Wolf', 150, 7),
+('She Thinks I Wont Stay Tonight', 166, 8),
+('He Heard Youre Bad For Me', 154, 8),
+('He Hopes We Cant Stay', 210, 8),
+('I Know I Know', 117, 8),
+('Hes Walking Away', 159, 9),
+('Hes Trouble', 138, 9),
+('I Heard I Want To Bo Alone', 120, 9),
+('I Ride Alone', 151, 9),
+('Honey', 79, 10),
+('You Cheated On Me', 95, 10),
+('Wouldnt It Be Nice', 213, 10),
+('Baby', 136, 10),
+('You Make Me Feel So..', 83, 10);
 
-INSERT INTO users (name, age, id_plan, signature_date)
-VALUES
-  ("Thati", 23, 1, 2019-10-20),
-  ("Cintia", 35, 2, 2017-12-30),
-  ("Bill", 20, 3, 2019-06-05),
-  ("Roger", 45, 4, 2020-05-13),
-  ("Norman", 58, 4, 2017-02-17),
-  ("Patrick", 33, 2, 2017-01-06),
-  ("Vivian", 26, 3, 2018-01-05),
-  ("Carol", 19, 3, 2018-02-14),
-  ("Angelina", 42, 2, 2018-04-29),
-  ("Paul", 46, 2, 2017-01-17);
+CREATE TABLE seguindo_artista(
+	usuario_id INT,
+    artista_id INT,
+    CONSTRAINT PRIMARY KEY(usuario_id, artista_id),
+    FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id),
+    FOREIGN KEY(artista_id) REFERENCES artistas(artista_id)
+);
 
-INSERT INTO plans (plan, plan_price)
-VALUES
-  ("gratuito", 0.00),
-  ("familiar", 7.99),
-  ("universitario", 5.99),
-  ("pessoal", 6.99);
+INSERT INTO seguindo_artista(usuario_id, artista_id) VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 1),
+(3, 2),
+(4, 4),
+(5, 5),
+(5, 6),
+(6, 6),
+(6, 1),
+(6, 3),
+(7, 2),
+(7, 5),
+(8, 1),
+(8, 5),
+(9, 6),
+(9, 3),
+(9, 4),
+(10, 6),
+(10, 2);
 
-INSERT INTO artists (artist)
-VALUES
-  ("Walter Phoenix"),
-  ("Lance Day"),
-  ("Peter Strong"),
-  ("Freedie Shannon"),
-  ("Tyler Isle"),
-  ("Fog");
+CREATE TABLE historico_de_reproducoes(
+	usuario_id INT,
+    cancoes_id INT,
+    data_reproducao DATETIME,
+    CONSTRAINT PRIMARY KEY(usuario_id, cancoes_id, data_reproducao),
+    FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id),
+    FOREIGN KEY(cancoes_id) REFERENCES cancoes(cancoes_id)
+);
 
-INSERT INTO following (artist_id, user_id)
-VALUES
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (1, 6),
-  (1, 8),
-  (2, 1),
-  (2, 2),
-  (2, 6),
-  (2, 9),
-  (3, 3),
-  (3, 7),
-  (3, 10),
-  (4, 1),
-  (4, 4),
-  (4, 9),
-  (5, 5),
-  (5, 7),
-  (5, 8),
-  (6, 5),
-  (6, 6),
-  (6, 9),
-  (6, 10),
-
-INSERT INTO albuns (album, artist_id, launch)
-VALUES
-  ("Envious", 1, 1990),
-  ("Exuberant", 1, 1993),
-  ("Hallowed Steam", 3, 1995),
-  ("Incandescent", 2, 1998),
-  ("Temporary Culture", 4, 2001),
-  ("Library of liberty", 4, 2003),
-  ("Chained Down", 5, 2007),
-  ("Cabinet of fools", 5, 2012),
-  ("No guarantees", 5, 2015),
-  ("Apparatus", 6, 2015);
-
-INSERT INTO history (user_id, music_id, reprodution_date)
-VALUES
-  (1, 10, "2020-02-28 10:45:55"),
-  (1, 28, "2020-05-02 05:30:35"),
-  (1, 27, "2020-03-06 11:22:33"),
-  (1, 20, "2020-08-05 08:05:17"),
-  (1, 21, "2020-09-14 16:32:22"),
-  (2, 35, "2020-01-02 07:40:33"),
-  (2, 7, "2020-05-16 06:16:22"),
-  (2, 25, "2020-10-09 12:27:48"),
-  (2, 39, "2020-09-21 13:14:46"),
-  (3, 3, "2020-11-13 16:55:13"),
-  (3, 12, "2020-12-05 18:38:30"),
-  (3, 29, "2020-07-30 10:00:00"),
-  (4, 11, "2021-08-15 17:10:10"),
-  (4, 36, "2021-07-10 15:20:30"),
-  (4, 30, "2021-01-09 01:44:33"),
-  (5, 14, "2020-07-03 19:33:28"),
-  (5, 18, "2017-02-24 21:14:22"),
-  (5, 20, "2020-08-06 15:23:43"),
-  (5, 1, "2020-11-10 13:52:27"),
-  (6, 38, "2019-02-07 20:33:48"),
-  (6, 31, "2017-01-24 00:31:17"),
-  (6, 32, "2017-10-12 12:35:20"),
-  (6, 26, "2018-05-29 14:56:41"),
-  (7, 13, "2018-05-09 22:30:49"),
-  (7, 2, "2020-07-27 12:52:58"),
-  (7, 17, "2018-01-16 18:40:43"),
-  (8, 39, "2018-03-21 16:56:40"),
-  (8, 40, "2020-10-18 13:38:05"),
-  (8, 9, "2019-05-25 08:14:03"),
-  (8, 34, "2021-08-15 21:37:09"),
-  (9, 5, "2021-05-24 17:23:45"),
-  (9, 22, "2018-12-07 22:48:52"),
-  (9, 15, "2021-03-14 06:14:26"),
-  (9, 16, "2020-04-01 03:36:00"),
-  (10, 24, "2017-02-06 08:21:34"),
-  (10, 25, "2017-12-04 05:33:43"),
-  (10, 18, "2017-07-27 05:24:49"),
-  (10, 19, "2017-12-25 01:03:57");
-
-INSERT INTO musics (music, album_id,  time)
-VALUES
-  ("Soul For Us", 1, 200),
-  ("Troubles Of My Inner Fire", 2, 203),
-  ("Magic Circus", 3, 105),
-  ("Fantasy For Me", 4, 100),
-  ("Thang Of Thunder", 5, 240),
-  ("Finding My Traditions", 7, 179),
-  ("She Thinks I Won't Stay Tonight", 8, 166),
-  ("He's Walking Away", 9, 159),
-  ("Honey", 10, 79),
-  ("Reflections Of Magic", 1, 163),
-  ("Dance With Her Own", 1, 116),
-  ("Time Fireworks", 2, 152),
-  ("Honey, So Do I", 3, 207),
-  ("Sweetie, Let's Go Wild", 3, 139),
-  ("She Knows", 3, 244),
-  ("Celebration Of More", 4, 146),
-  ("Rock His Everything", 4, 223),
-  ("Home Forever", 4, 231),
-  ("Diamond Power", 4, 241),
-  ("Let's Be Silly", 4, 132),
-  ("Words Of Her Life", 5, 185),
-  ("Without My Streets", 5, 176),
-  ("History Of My Roses", 6, 222),
-  ("Without My Love", 6, 111),
-  ("Walking And Game", 6, 123),
-  ("Young And Father", 6, 197),
-  ("Walking And Man", 7, 229),
-  ("Hard And Time", 7, 135),
-  ("Honey, I'm A Lone Wolf", 7, 150),
-  ("He Heard You're Bad For Me", 8, 154),
-  ("He Hopes We Can't Stay", 8, 210),
-  ("I Know I Know", 8, 117),
-  ("He's Trouble", 9, 138),
-  ("I Heard I Want To Bo Alone", 9, 120),
-  ("Ride Alone", 9, 151),
-  ("You Cheated On Me", 10, 95),
-  ("Wouldn't It Be Nice", 10, 213),
-  ("Baby", 10, 136),
-  ("You Make Me Feel So", 10, 83);
+INSERT INTO historico_de_reproducoes(usuario_id, cancoes_id, data_reproducao) VALUES
+(1, 36, '2020-02-28 10:45:55'),
+(1, 25, '2020-05-02 05:30:35'),
+(1, 23, '2020-03-06 11:22:33'),
+(1, 14, '2020-08-05 08:05:17'),
+(1, 15, '2020-09-14 16:32:22'),
+(2, 34, '2020-01-02 07:40:33'),
+(2, 24, '2020-05-16 06:16:22'),
+(2, 21, '2020-10-09 12:27:48'),
+(2, 39, '2020-09-21 13:14:46'),
+(3, 6, '2020-11-13 16:55:13'),
+(3, 3, '2020-12-05 18:38:30'),
+(3, 26, '2020-07-30 10:00:00'),
+(4, 2, '2021-08-15 17:10:10'),
+(4, 35, '2021-07-10 15:20:30'),
+(4, 27, '2021-01-09 01:44:33'),
+(5, 7, '2020-07-03 19:33:28'),
+(5, 12, '2017-02-24 21:14:22'),
+(5, 14, '2020-08-06 15:23:43'),
+(5, 1, '2020-11-10 13:52:27'),
+(6, 38, '2019-02-07 20:33:48'),
+(6, 29, '2017-01-24 00:31:17'),
+(6, 30, '2017-10-12 12:35:20'),
+(6, 22, '2018-05-29 14:56:41'),
+(7, 5, '2018-05-09 22:30:49'),
+(7, 4, '2020-07-27 12:52:58'),
+(7, 11, '2018-01-16 18:40:43'),
+(8, 39, '2018-03-21 16:56:40'),
+(8, 40, '2020-10-18 13:38:05'),
+(8, 32, '2019-05-25 08:14:03'),
+(8, 33, '2021-08-15 21:37:09'),
+(9, 16, '2021-05-24 17:23:45'),
+(9, 17, '2018-12-07 22:48:52'),
+(9, 8, '2021-03-14 06:14:26'),
+(9, 9, '2020-04-01 03:36:00'),
+(10, 20, '2017-02-06 08:21:34'),
+(10, 21, '2017-12-04 05:33:43'),
+(10, 12, '2017-07-27 05:24:49'),
+(10, 13, '2017-12-25 01:03:57');
